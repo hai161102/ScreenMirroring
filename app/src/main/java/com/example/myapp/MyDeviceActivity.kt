ca33e1fef7b1923e15842aab90f2c7655e6d93f5
1451020071@dnu.edu.vn <1451020071@dnu.edu.vn>
2022-04-18 10:55:45 +0700

    update UI

--- app/src/main/java/com/example/myapp/MyDeviceActivity.kt
+++ app/src/main/java/com/example/myapp/MyDeviceActivity.kt
@@ -1,27 +1,21 @@
 package com.example.myapp
 
-import android.app.AlertDialog
-import android.app.Dialog
-import android.content.DialogInterface
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
-import kotlinx.android.synthetic.main.activity_mydevice.view.*
 
-class MyDeviceActivity : DialogFragment()  {
+class MyDeviceActivity : DialogFragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val rootView : View = inflater.inflate(R.layout.activity_mydevice,container, false)
-//        rootView.refresh.setOnClickListener {
-//
-//        }
-        return rootView
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+        return inflater.inflate(R.layout.activity_mydevice, container, false)
     }
 }
